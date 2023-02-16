da8c4929adc3736001d90575f9ac1343adab8d60
Owen Stubbs <owen77stubbs@gmail.com>
2023-02-16 05:51:33 +0000

    Update Test.java

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,5 +1,8 @@
-import com.google.gson.JsonArray;
-import net.emc.emcw.utils.API;
+import com.google.gson.JsonObject;
+import net.emc.emcw.core.EMCMap;
+import net.emc.emcw.core.EMCWrapper;
+import net.emc.emcw.objects.Player;
+import net.emc.emcw.utils.GsonUtil;
 
 public class Test {
     static void sleep(int ms){
@@ -8,8 +11,12 @@ public class Test {
     }
 
     public static void main(String[] args) {
-        JsonArray players = API.playerData("aurora");
+        EMCMap aurora = new EMCWrapper().Aurora;
+        JsonObject firstOp = (JsonObject) aurora.onlinePlayers().asList().get(1);
+
         sleep(1400);
-        System.out.println("Output: \n" + players);
+
+        Player p = aurora.getOnlinePlayer(firstOp);
+        System.out.println("Output: \n" + GsonUtil.stringify(p));
     }
 }

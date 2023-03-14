833c1fc5d966974f5ced5eff7aa4941ba401e92d
Owen Stubbs <owen77stubbs@gmail.com>
2023-03-14 21:02:34 +0000

    Update Collective.java

--- src/main/java/io/github/emcw/interfaces/Collective.java
+++ src/main/java/io/github/emcw/interfaces/Collective.java
@@ -1,9 +1,11 @@
 package io.github.emcw.interfaces;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.*;
 
 public interface Collective<T> {
-    default T single(String key, Map<String, T> map) throws NullPointerException {
+    default T single(String key, @NotNull Map<String, T> map) throws NullPointerException {
         return map.getOrDefault(key, null);
     }
 }

8d708443421807500243c0d1ae298a65cab45afe
Owen Stubbs <owen77stubbs@gmail.com>
2023-03-14 21:02:08 +0000

    Update Collective.java

--- src/main/java/io/github/emcw/interfaces/Collective.java
+++ src/main/java/io/github/emcw/interfaces/Collective.java
@@ -2,23 +2,8 @@ package io.github.emcw.interfaces;
 
 import java.util.*;
 
-import static io.github.emcw.utils.GsonUtil.streamEntries;
-
 public interface Collective<T> {
     default T single(String key, Map<String, T> map) throws NullPointerException {
         return map.getOrDefault(key, null);
-    }
-
-    default T merge(Map<String, T> map1, Map<String, T> map2) {
-        // In parallel
-        streamEntries(map2).forEach(e -> {
-
-        });
-
-        // Loop through map2
-        // Exists in map1, merge.
-        // Doesn't exist in map1, put.
-
-        return null;
     }
 }
